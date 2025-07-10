00a20eab6c21b35d2256fac79eb05067771a9fce
yangfengshan3 <yangfengshan2@jd.com>
2025-07-10 13:37:27 +0800

    1

--- spring-ai-webflux/src/main/java/com/cdd/demo/spring_ai_webFlux/application/web/ChatController.java
+++ spring-ai-webflux/src/main/java/com/cdd/demo/spring_ai_webFlux/application/web/ChatController.java
@@ -1,13 +1,17 @@
 package com.cdd.demo.spring_ai_webFlux.application.web;
 
+import io.modelcontextprotocol.client.McpAsyncClient;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.mcp.AsyncMcpToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
 import java.util.function.Function;
 
 @RestController
@@ -20,7 +24,8 @@ public class ChatController {
     private ChatClient ollamaClient;
     @Resource(name = "responseFilter")
     private Function<ChatResponse, String> responseFilter;
-
+    @Autowired
+    private List<McpAsyncClient> mcpAsyncClients;
     /**
      * 多模型调用 流式输出
      * 可以推理模型帮你写图片描述，调图片生成模型生成图片
@@ -30,7 +35,10 @@ public class ChatController {
      */
     @RequestMapping("/chat")
     public Flux<String> chat1(String message) {
-        Flux<String> firstFlux = ollamaClient.prompt().user(message).stream().content().cache();
+        Flux<String> firstFlux = ollamaClient
+                .prompt()
+                .toolCallbacks(new AsyncMcpToolCallbackProvider(mcpAsyncClients))
+                .user(message).stream().content().cache();
         return firstFlux;
     }
 }
--- spring-ai-webflux/src/main/java/com/cdd/demo/spring_ai_webFlux/application/web/client/chat/OllamaClientConfig.java
+++ spring-ai-webflux/src/main/java/com/cdd/demo/spring_ai_webFlux/application/web/client/chat/OllamaClientConfig.java
@@ -1,19 +1,28 @@
 package com.cdd.demo.spring_ai_webFlux.application.web.client.chat;
 
+import io.modelcontextprotocol.client.McpAsyncClient;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.mcp.AsyncMcpToolCallbackProvider;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OllamaClientConfig {
+    @Autowired
+    private List<McpAsyncClient> mcpAsyncClients;
     @Bean("ollamaClient")
     ChatClient ollamaClient(OllamaChatModel model) {
         return ChatClient.builder(model)
                 .defaultSystem(
                         "你是游玩规划员。")
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                //此时设置 工具方法 初始化时调用 不存在阻塞不阻塞
+                .defaultToolCallbacks(new AsyncMcpToolCallbackProvider(mcpAsyncClients))
                 .build();
     }
 }
