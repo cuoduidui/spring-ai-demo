f4535f0c1fa7717ff2d9989ac9ebcacb1c7f1260
yangfengshan3 <yangfengshan2@jd.com>
2025-07-15 10:06:05 +0800

    自定义mcp工具

---
+++ spring-ai-webflux-mcp-server-sse/src/main/java/com/cdd/demo/spring-ai-webflux-mcp-server-sse/mcp_server/web/config/McpServerConfig.java
@@ -0,0 +1,6 @@
+package com.cdd.demo.spring_ai_webflux_mcp_server_stdio.mcp_server_stdio.web.config;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class McpServerConfig {
+}
---
+++ spring-ai-webflux-mcp-server-sse/src/main/java/com/cdd/demo/spring-ai-webflux-mcp-server-sse/mcp_server/web/tools/server/McpServerService.java
@@ -0,0 +1,22 @@
+package com.example.ai.demo_ai_webFlux.mcp_server.web.tools.server;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+
+@Service
+public class McpServerService {
+
+
+    /**
+     * 通过时间获取一个行动代号
+     * @param data data
+     * @return 返回一个代号
+     * @throws RestClientException if the request fails
+     */
+    @Tool(description = "通过时间获取一个行动代号")
+    public String getActionMark(@ToolParam(description = "日期") String data) {
+        return "代号：牛逼" + data;
+    }
+}
